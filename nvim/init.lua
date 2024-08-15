be1014a12ad1997faa909cc458caa94d61acc1b3
Dan Kirkham <daniel.a.kirkham@gmail.com>
2024-08-14 20:35:17 -0500

    Fix unselect. Add spade plugin. Add status bar.

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,4 +49,45 @@ vim.keymap.set('n', '<leader>t', ':set noet<CR>')
 vim.keymap.set('n', '<leader>2', ':set sw=2 ts=2<CR>')
 vim.keymap.set('n', '<leader>4', ':set sw=4 ts=4<CR>')
 
-vim.keymap.set('n', '<leader>u', ':noh')
+vim.keymap.set('n', '<leader>u', ':noh<CR>')
+
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'jellybeans',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
