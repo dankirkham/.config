0845d379c83f84833c9893c938fa0f707b68faed
Dan Kirkham <dkirkham@trideum.com>
2024-10-29 12:51:01 -0500

    dont use appimage

--- scripts/install_neovim.sh
+++ scripts/install_neovim.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 echo Installing neovim...
-wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 mkdir -p ~/.local/
-mkdir -p ~/.local/bin
-mv nvim.appimage ~/.local/bin/nvim
+tar xvf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+mv nvim-linux64/* ~/.local/
